f495ac699c3999819788e24fa6e58cee4c1a6ab5
= <=>
2021-03-16 18:35:46 +0100

    0.1.3

--- projects/angular-utils/src/lib/business.ts
+++ projects/angular-utils/src/lib/business.ts
@@ -2,7 +2,7 @@ import { BusinessInfo } from './business-info';
 import { WeekAvailability } from './week-availability';
 
 export class Business {
+    id?: string;
     info: BusinessInfo;
     availability: WeekAvailability;
-
 }
